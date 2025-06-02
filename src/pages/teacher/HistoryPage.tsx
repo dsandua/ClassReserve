48ca5d832263d56995b4e787926a9b58d2357f0e
David Sandua <dsandua@gmail.com>
2025-06-02 19:07:33 +0200

    Updated HistoryPage.tsx

--- src/pages/teacher/HistoryPage.tsx
+++ src/pages/teacher/HistoryPage.tsx
@@ -222,7 +222,7 @@ const HistoryPage = () => {
                     <td className="px-6 py-4 whitespace-nowrap">
                       <div className="flex items-center">
                         <div className="flex-shrink-0 h-10 w-10">
-                          {booking.studentName.charAt(0)}
+                          booking.studentName?.charAt(0) ?? ''
                         </div>
                         <div className="ml-4">
                           <div className="text-sm font-medium text-gray-900">{booking.studentName}</div>
