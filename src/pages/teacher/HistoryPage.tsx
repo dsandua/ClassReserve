e783ca3d4d87029e78830f63a53e9768eb2bb76e
David Sandua <dsandua@gmail.com>
2025-06-05 20:03:22 +0200

    Updated HistoryPage.tsx

--- src/pages/teacher/HistoryPage.tsx
+++ src/pages/teacher/HistoryPage.tsx
@@ -297,7 +297,7 @@ const HistoryPage = () => {
                       </div>
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">{booking.startTime} - {booking.endTime}</div>
+                      <div className="text-sm text-gray-900">{booking.startTime?.slice(0, 5) } - {booking.endTime?.slice(0, 5) }</div>
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
                       <div className="relative">
